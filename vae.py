b1dd86b9edb98d4d61d9d1af3fe18e86afab823b
Atin Sakkeer Hussain <atinsaki@gmail.com>
2025-01-01 22:40:17 +0400

    Convert TensorFlow to PyTorch

--- vae.py
+++ vae.py
@@ -1,132 +1,147 @@
-from __future__ import print_function
-
 import warnings as w
-w.simplefilter(action = 'ignore')
+
+w.simplefilter(action="ignore")
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
-
-from keras.layers import Input, Dense, Lambda
-from keras.models import Model
-from keras import backend as K
-from keras import metrics
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
 import os
-from PIL import Image
 import cv2
+from tqdm.auto import tqdm
 
+parser = argparse.ArgumentParser(description="Variational Autoencoder")
+parser.add_argument(
+    "--input", type=str, required=True, help="Directory containing images eg: data/"
+)
+parser.add_argument("--name", type=str, required=True, help="Name for the model")
+parser.add_argument(
+    "--epoch", type=int, default=500, help="Number of training iterations"
+)
+parser.add_argument("--batch", type=int, default=100, help="Batch Size")
+parser.add_argument(
+    "--original_dim", type=int, default=128, help="Dimension of Intermediate Layer"
+)
+parser.add_argument(
+    "--inter_dim", type=int, default=256, help="Dimension of Intermediate Layer"
+)
+args = parser.parse_args()
 
-batch_size = 100
-original_dim = 12288
+# Parameters
+epochs = args.epoch
+intermediate_dim = args.inter_dim
+batch_size = args.batch
+original_dim = args.original_dim * args.original_dim * 3
 latent_dim = 2
-intermediate_dim = 256
-epochs = 500
 epsilon_std = 1.0
 
 
-parser = argparse.ArgumentParser(description='Variational Autoencoder')
-parser.add_argument('--input',type=str,required=True,help='Directory containing images eg: data/')
-parser.add_argument('--epoch',type=int,default=500,help='No of training iterations')
-parser.add_argument('--batch',type=int,default=100,help='Batch Size')
-parser.add_argument('--inter_dim',type=int,default=256,help='Dimension of Intermediate Layer')
-args = parser.parse_args()
+# Custom Dataset
+class ImageDataset(Dataset):
+    def __init__(self, img_dir):
+        self.img_paths = [os.path.join(img_dir, f) for f in os.listdir(img_dir)]
 
-epochs = args.epoch
-intermediate_dim = args.inter_dim
+    def __len__(self):
+        return len(self.img_paths)
 
-x = Input(shape=(original_dim,))
-h = Dense(intermediate_dim, activation='relu')(x)
-z_mean = Dense(latent_dim)(h)
-z_log_var = Dense(latent_dim)(h)
-
-
-def sampling(args):
-    z_mean, z_log_var = args
-    epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim), mean=0.,
-                              stddev=epsilon_std)
-    return z_mean + K.exp(z_log_var / 2) * epsilon
-
-# note that "output_shape" isn't necessary with the TensorFlow backend
-z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
-
-# we instantiate these layers separately so as to reuse them later
-decoder_h = Dense(intermediate_dim, activation='relu')
-decoder_mean = Dense(original_dim, activation='sigmoid')
-h_decoded = decoder_h(z)
-x_decoded_mean = decoder_mean(h_decoded)
+    def __getitem__(self, idx):
+        img = cv2.imread(self.img_paths[idx])
+        img = img.astype("float32") / 255.0
+        img = cv2.resize(img, (args.original_dim, args.original_dim))
+        return torch.tensor(img, dtype=torch.float32).view(-1)
 
 
+# VAE Model
+class VAE(nn.Module):
+    def __init__(self, original_dim, intermediate_dim, latent_dim):
+        super(VAE, self).__init__()
+        self.fc1 = nn.Linear(original_dim, intermediate_dim)
+        self.fc2_mean = nn.Linear(intermediate_dim, latent_dim)
+        self.fc2_log_var = nn.Linear(intermediate_dim, latent_dim)
+        self.fc3 = nn.Linear(latent_dim, intermediate_dim)
+        self.fc4 = nn.Linear(intermediate_dim, original_dim)
+
+    def encode(self, x):
+        h = F.relu(self.fc1(x))
+        return self.fc2_mean(h), self.fc2_log_var(h)
+
+    def reparameterize(self, mean, log_var):
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(std)
+        return mean + eps * std
+
+    def decode(self, z):
+        h = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h))
+
+    def forward(self, x):
+        mean, log_var = self.encode(x)
+        z = self.reparameterize(mean, log_var)
+        return self.decode(z), mean, log_var
 
 
-# train the VAE on MNIST digits
-#(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-y_train = []
-y_test =[]
-
-for each in os.listdir(args.input):
-    img = cv2.imread(os.path.join(args.input,each))
-    np_im = np.array(img)
-    y_train.append(img)
-    y_test.append(img)
-
-x_train=np.array(y_train)
-x_test=np.array(y_test)
-
-print(x_train.shape)
-
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-
-# instantiate VAE model
-vae = Model(x, x_decoded_mean)
-
-# Compute VAE loss
-xent_loss = original_dim * metrics.binary_crossentropy(x, x_decoded_mean)
-kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
-vae_loss = K.mean(xent_loss + kl_loss)
-
-vae.add_loss(vae_loss)
-vae.compile(optimizer='rmsprop')
-
-vae.summary()
-
-vae.fit(x_train,
-        shuffle=True,
-        epochs=epochs,
-        batch_size=batch_size,
-        validation_data=(x_test, None))
-
-# build a model to project inputs on the latent space
-encoder = Model(x, z_mean)
-
-x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
+# Loss Function
+def loss_function(recon_x, x, mean, log_var):
+    BCE = F.binary_cross_entropy(recon_x, x, reduction="sum")
+    KLD = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
+    return BCE + KLD
 
 
-# build a image generator that can sample from the learned distribution
-decoder_input = Input(shape=(latent_dim,))
-_h_decoded = decoder_h(decoder_input)
-_x_decoded_mean = decoder_mean(_h_decoded)
-generator = Model(decoder_input, _x_decoded_mean)
+# Load Data
+train_dataset = ImageDataset(args.input)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
+# Initialize Model
+vae = VAE(original_dim, intermediate_dim, latent_dim).to(
+    torch.device("cuda" if torch.cuda.is_available() else "cpu")
+)
+optimizer = optim.Adam(vae.parameters(), lr=1e-3)
 
+if os.path.exists(f"{args.name}.h5"):
+    vae.load_state_dict(torch.load(f"{args.name}.h5", map_location=torch.device("cpu")))
 
-n = 20  
-digit_size = 64
-figure = np.zeros((digit_size * n, digit_size * n, 3 * n))
-# linearly spaced coordinates on the unit square were transformed through the inverse CDF (ppf) of the Gaussian
-# to produce values of the latent variables z, since the prior of the latent space is Gaussian
-grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
-grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
+# Training Loop
+vae.train()
+pbar = tqdm(range(epochs))
+for epoch in pbar:
+    train_loss = 0
+    for batch in train_loader:
+        optimizer.zero_grad()
+        batch = batch.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+        recon_batch, mean, log_var = vae(batch)
+        loss = loss_function(recon_batch, batch, mean, log_var)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
 
+    pbar.set_description_str(
+        f"Epoch {epoch + 1}/{epochs}, Loss: {train_loss / len(train_loader.dataset):.4f}"
+    )
+torch.save(vae.state_dict(), f"{args.name}.h5")
 
-for i, yi in enumerate(grid_x):
-    for j, xi in enumerate(grid_y):
-        z_sample = np.array([[xi, yi]])
-        x_decoded = generator.predict(z_sample)
-        newimage = x_decoded[0].reshape(digit_size, digit_size, 3)
-        img_cv = cv2.resize(newimage,(256,256))
-        cv2.imshow('Color image', img_cv)
-        cv2.waitKey(0)
+# Generate Images
+vae.eval()
+with torch.no_grad():
+    n = 4
+    digit_size = args.original_dim
+    figure = np.zeros((digit_size * n, digit_size * n, 3))
+    grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
+    grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
+
+    for i, yi in enumerate(grid_x):
+        for j, xi in enumerate(grid_y):
+            z_sample = torch.tensor([[xi, yi]], dtype=torch.float32)
+            x_decoded = vae.decode(z_sample).numpy()
+            newimage = x_decoded.reshape(digit_size, digit_size, 3)
+            figure[
+                i * digit_size : (i + 1) * digit_size,
+                j * digit_size : (j + 1) * digit_size,
+            ] = newimage
+
+    plt.imshow(figure)
+    plt.axis("off")
+    plt.show()
